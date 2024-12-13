966658014b52a0ace5c87f6c8c5943e2a4ceb6d6
Guanart <gu4n4rt@gmail.com>
2024-12-13 01:07:51 -0300

    fix firewall puerto 5001

--- TP-Integrador/terraform/07-firewalls.tf
+++ TP-Integrador/terraform/07-firewalls.tf
@@ -5,7 +5,7 @@ resource "google_compute_firewall" "allow-http-https-ssh-rabbit" {
 
     allow {
         protocol = "tcp"
-        ports    = ["22", "80", "443", "5672", "5000", "5001", "8001", "15672"]
+        ports    = ["22", "80", "443", "5672", "5000", "8001", "15672"]
     }
 
     source_ranges = ["0.0.0.0/0"]
