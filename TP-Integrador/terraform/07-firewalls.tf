71732e5307cb7dcf77a6659d96d9c62aea2e998b
Gonzalo Benito <gu4n4rt@gmail.com>
2024-12-16 14:50:13 -0300

    regla firewall para workers

--- TP-Integrador/terraform/07-firewalls.tf
+++ TP-Integrador/terraform/07-firewalls.tf
@@ -10,3 +10,16 @@ resource "google_compute_firewall" "allow-http-https-ssh-rabbit" {
 
     source_ranges = ["0.0.0.0/0"]
 }
+
+# Firewall para workers en la nube
+resource "google_compute_firewall" "allow-http-https-ssh" {
+    name    = "allow-http-https-ssh-rabbit-flask"
+    network = "default"
+
+    allow {
+        protocol = "tcp"
+        ports    = ["22", "80", "443", "5000", "5001", "5002", "5672", "15672"]
+    }
+
+    source_ranges = ["0.0.0.0/0"]
+}
