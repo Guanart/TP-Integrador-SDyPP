5bcb9affb47bc46e75855aae562b8ac78733de00
Guanart <gu4n4rt@gmail.com>
2024-12-16 21:06:37 -0300

    [deploy-worker-pool] terraform para deployar worker pool

---
+++ TP-Integrador/worker-pool/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_instance" "worker_pool_manager" {
+  name         = "worker-pool-manager"
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    git clone https://github.com/Guanart/SDyPP-2024-grupo-4.git
+    echo "${var.metadata_startup_script}" > /home/packer/SDyPP-2024-grupo-4/TP-Integrador/docker-compose-worker-pool/.env
+    cd /home/packer/SDyPP-2024-grupo-4/TP-Integrador/docker-compose-worker-pool
+    docker compose up -d
+  EOT
+}
